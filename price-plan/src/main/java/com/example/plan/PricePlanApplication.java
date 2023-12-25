02c83b52490a3d01160f2942be90aa49f144793d
yaans <kbeeysk@naver.com>
2023-12-26 00:38:49 +0900

    init : price plan project

---
+++ price-plan/src/main/java/com/example/plan/PricePlanApplication.java
@@ -0,0 +1,13 @@
+package com.example.plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricePlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricePlanApplication.class, args);
+    }
+
+}
